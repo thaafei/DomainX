e4eb4db685df9f59a6d38ba47c28f929e6ec71a3
hamidizf <haniyehamidizadeh@gmail.com>
2026-01-22 22:47:28 -0500

    updated default autofield to test on docker

--- src/backend/DomainX/settings.py
+++ src/backend/DomainX/settings.py
@@ -239,4 +239,4 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.model'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
