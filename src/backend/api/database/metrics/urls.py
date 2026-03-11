4cb266ea115f952bc8c990bb998679ca69aac695
hamidizf <haniyehamidizadeh@gmail.com>
2026-03-10 21:10:04 -0400

    fetch github metrics

--- src/backend/api/database/metrics/urls.py
+++ src/backend/api/database/metrics/urls.py
@@ -6,13 +6,15 @@ from .views import (
     MetricListFlatView,
     MetricRulesView,
     MetricCategoryView,
+    AutoMetricOptionsView,
 )
 
 urlpatterns = [
     path("", MetricListCreateView.as_view(), name="metric-list-create"),
-    path("<uuid:metric_id>/", MetricRetrieveUpdateDestroyView.as_view(), name="metric-detail"),
-    path("<uuid:metric_id>/update-weight/", MetricUpdateWeightView.as_view(), name="metric-update-weight"),
+    path("auto-options/", AutoMetricOptionsView.as_view(), name="metric-auto-options"),
     path("all/", MetricListFlatView.as_view(), name="metric-all-flat"),
     path("rules/", MetricRulesView.as_view(), name="metric-rules"),
     path("categories/", MetricCategoryView.as_view(), name="metric-categories"),
+    path("<uuid:metric_id>/update-weight/", MetricUpdateWeightView.as_view(), name="metric-update-weight"),
+    path("<uuid:metric_id>/", MetricRetrieveUpdateDestroyView.as_view(), name="metric-detail"),
 ]
