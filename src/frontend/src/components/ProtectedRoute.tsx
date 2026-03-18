564c54a1425ebaf8bd25641b063dbe51b9a3fcaa
hamidizf <haniyehamidizadeh@gmail.com>
2026-03-18 10:45:23 -0400

    remove back to main

--- src/frontend/src/components/ProtectedRoute.tsx
+++ src/frontend/src/components/ProtectedRoute.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
 import { useAuthStore } from '../store/useAuthStore';
+import AuthTransition from './AuthTransition';
 
 interface ProtectedRouteProps {
   children: React.ReactNode;
 }
 
 const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
-  const { user } = useAuthStore();
+  const { user, isLoading } = useAuthStore();
   const location = useLocation();
+
+  if (isLoading) {
+    return <AuthTransition />;
+  }
 
   if (!user) {
     return <Navigate to="/login" state={{ from: location.pathname }} replace />;
